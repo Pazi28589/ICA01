e20873c0e4d1a107b8cff99ed4ebf6ec83d58433
Pasindu Ravishka Hettiarachchi <154882714+Pazi28589@users.noreply.github.com>
2024-12-03 22:05:41 +0530

    Update Products.js

--- ica1/src/components/Products.js
+++ ica1/src/components/Products.js
@@ -1,8 +1,25 @@
-
 import '../assets/CSS/layout.css';
+import Product from './Product';
+import Cart from './Cart';
+import { flowers } from './FlowerDB';
+import {useState} from 'react'
 
 
 export default function Products(){
+     const [cart,setCart]=useState([]);
+
+     const AddToCart =(flower) =>{
+        const existingItem = cart.find((item) => item.id === flower.id)
+        if(existingItem){
+            setCart(
+                cart.map((item) =>
+                    item.id === flower.id ? {...item,qty: item.qty + flower.qty} : item
+                )
+            );
+        }else{
+            setCart([...cart,{...flower}])
+        }
+     }
    
     return(
         <>
@@ -13,15 +30,21 @@ export default function Products(){
                 <h4 className="card-title">Buy flowers</h4>
                 <div className="grid-container">
                     {
-                        //product
+                        flowers.map((flower)=>(
+                            <Product 
+                               key={flower.id}
+                               flower={flower}
+                               onAddToCart={AddToCart}
+                               />
+
+                        )
+                    )
                     }
                 </div>
 
             </div>
             <div className="item3">
-                {
-                //cart
-                }
+                <Cart cart={cart}/>
             </div>
         </>
     );
